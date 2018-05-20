bccc5feaf1ff3ddb1cbb8389b4514d7b0400d9a4
zenmaibane <koteicream.1024@gmail.com>
2018-05-20 22:58:38 +0900

    プリッカソンの成果物

---
+++ Assets/Sword.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+	private Vector3 preMousePos;
+    void Start()
+    {
+		preMousePos = Input.mousePosition;
+    }
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 deltaMouse = mousePos - preMousePos;
+        transform.Rotate(0, deltaMouse.x * 0.1f, deltaMouse.y * -0.1f);
+        preMousePos = mousePos;
+    }
+}
---
+++ Assets/destroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    public GameObject breakPrefab;
+    void OnCollisionEnter(Collision c)
+    {
+        Instantiate(breakPrefab, gameObject.transform.position, breakPrefab.transform.rotation);
+        Destroy(gameObject, 0.05f);
+    }
+}
